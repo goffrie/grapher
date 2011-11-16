41f6dc692a29a0248935a067b4d68342f28cfdab
Geoffry Song <goffrie@gmail.com>
2011-11-16 18:44:09 -0500

    remove unused portions of Boost

--- util.h
+++ util.h
@@ -1,13 +1,13 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
-inline bool isIntegral(Number a) {
-    const Number b = floor(a + 0.5);
-    const Number epsilon = 1e-4;
+inline constexpr bool isIntegral(Number a) {
+    constexpr Number b = floor(a + 0.5);
+    constexpr Number epsilon = 1e-4;
     return (a < b + epsilon) && (b < a + epsilon);
 }
 
-inline int rnd(Number a) {
+inline constexpr int rnd(Number a) {
     return static_cast<int>(a + 0.5);
 }
 
