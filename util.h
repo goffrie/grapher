e02b2dbfca08610ab984a5807629fcadd847ce30
Geoffry Song <goffrie@gmail.com>
2011-11-17 20:59:43 -0500

    stop lying about constexpr

--- util.h
+++ util.h
@@ -1,9 +1,9 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
-inline constexpr bool isIntegral(Number a) {
-    constexpr Number b = floor(a + 0.5);
-    constexpr Number epsilon = 1e-4;
+inline bool isIntegral(Number a) {
+    const Number b = floor(a + 0.5);
+    const Number epsilon = 1e-4;
     return (a < b + epsilon) && (b < a + epsilon);
 }
 
