ddc8da55f25f4dc758beb356fbeda9fbbb3e09c7
Geoffry Song <goffrie@gmail.com>
2013-01-12 15:09:53 -0500

    Slightly improve the documentation in Expression.h.

--- Expression.h
+++ Expression.h
@@ -30,19 +30,21 @@ namespace llvm {
     class ExecutionEngine;
     class Value;
     class Module;
-class Function;
+    class Function;
 }
 
 struct Variable;
 namespace std {
+    // to allow using Variable as a hashmap key
     template<> class hash<Variable> : public std::unary_function<Variable, size_t> {
     public:
         std::size_t operator()(const Variable& a) const;
     };
 }
+// precedence of operators
 namespace Precedence {
     enum {
-        Const = 1000,
+        Const = 1000, // constants have the highest "precedence"; brackets never needed around them
         Func = 100,
         Pow = 10,
         Div = 7,
@@ -54,35 +56,49 @@ namespace Precedence {
     };
 }
 
+/// Structure holding onto an LLVM JIT
 struct MathContext {
     llvm::ExecutionEngine* jitEngine;
     llvm::FunctionPassManager* fpm;
     llvm::Module* module;
+
+    /// Allocates a new default context.
     static MathContext defaultContext();
 };
 
+/// Base class for any mathematical object, such as an expression or an equation.
 struct Thing {
     virtual ~Thing() { };
+    /// Return a textual representation of this object, ideally parsable by Parser.
+    /// @param precedence The precedence of the outer expression. Influences whether the string is returned wrapped in parentheses.
     virtual std::string toString(int precedence = -1) const = 0;
 };
 struct Expression;
 struct Polynomial;
 typedef std::unique_ptr<Expression> EPtr;
 typedef double (*EvalFunc)();
-// typedef __v4sf (*VectorFunc)(int);
+
+/// Deletes a function.
+/// This function exists to avoid polluting Expression.h with more LLVM includes.
 void dispose(llvm::Function* f);
+
+/// RAII class for JIT-ted functions.
 template<typename T>
 class Wrap {
 private:
+    /// The LLVM function object.
     llvm::Function* lfunc;
+    /// Callable function pointer, obtained from the JIT compiler.
     T func;
 public:
     Wrap() : lfunc(nullptr), func(nullptr) { }
     Wrap(llvm::Function* l, T f) : lfunc(l), func(f) { }
     Wrap(Wrap<T>&& _f) : lfunc(_f.lfunc), func(_f.func) { _f.lfunc = nullptr; _f.func = nullptr; }
     ~Wrap() { dispose(lfunc); }
+
     template<typename... R>
     auto operator()(R&&... i) const -> decltype(func(i...)) { return func(std::forward<R>(i)...); }
+
     Wrap<T>& operator=(Wrap<T>&& _f) {
         std::swap(lfunc, _f.lfunc);
         std::swap(func, _f.func);
@@ -90,23 +106,42 @@ public:
     }
 };
 typedef Wrap<EvalFunc> WEvalFunc;
-// typedef Wrap<VectorFunc> WVectorFunc;
+
+/// Abstract base class for expressions operating on float values.
 struct Expression : public Thing {
     typedef std::unordered_map<Variable, Expression*> Subst;
+
+    /// Evaluate this expression as a scalar.
     virtual Number evaluate() const = 0;
+    /// Evaluate this expression as a vector of size \a size.
+    /// @return A newly allocated (using VECTOR_ALLOC) vector with the result of evaluating the expression.
     virtual Vector evaluateVector(uz size) const = 0;
+    /// JIT-compile this expression.
     WEvalFunc evaluator(MathContext& context) const;
+    /// Create an LLVM value object for this expression.
     virtual llvm::Value* evaluateJIT(llvm::IRBuilder<>& builder, MathContext& context) const = 0;
+    /// Recursively substitute the given variables with the bound expressions.
     virtual EPtr substitute(const Subst& s) const = 0;
+    /// Take the partial derivative of this expression, with respect to \a var.
     virtual EPtr derivative(const Variable& var) const = 0;
+    /// Try to simplify the expression, by e.g. constant folding.
     virtual EPtr simplify() const { return EPtr(copy()); }
+    /// Recursively list the variables bound in this expression.
     virtual void variables(std::set<Variable>& out) const = 0;
-    virtual Expression* copy() const = 0;
+    /// Check if this expression is polynomial in the given variable, treating all others as constant.
     virtual bool polynomial(const Variable&) const { return false; }
-    EPtr ecopy() const { return EPtr(copy()); }
+    /// Expand this expression (distribute multiplies over addition).
     virtual EPtr expand() const { return ecopy(); }
+    /// Turn this expression into a polynomial in terms of \a var.
+    /// Not well-defined if polynomial(var) would return false.
+    /// @return A newly-allocated Polynomial.
     virtual std::unique_ptr<Polynomial> facsum(const Variable& var) const;
+    /// Create a deep copy of this expression.
+    virtual Expression* copy() const = 0;
+    /// Create a deep copy, then wrap it in a std::unique_ptr.
+    EPtr ecopy() const { return EPtr(copy()); }
 };
+/// A scalar constant.
 struct Constant : public Expression {
     Number c;
     Constant(Number _c) : c(_c) { }
@@ -121,6 +156,8 @@ struct Constant : public Expression {
     bool polynomial(const Variable&) const { return true; }
     std::string toString(int prec = -1) const;
 };
+/// A variable. Can be bound to either a scalar or vector pointer,
+/// which is dereferenced at evaluate-time.
 struct Variable : public Expression {
     struct Id {
         std::string s;
@@ -166,6 +203,7 @@ namespace std {
         return std::hash<Variable::Id*>()(a.id.get());
     }
 }
+/// Abstract base class for unary operations, such as functions.
 struct UnaryOp : public Expression {
     EPtr a;
     UnaryOp(EPtr _a) : a(std::move(_a)) { }
@@ -179,8 +217,10 @@ struct UnaryOp : public Expression {
     EPtr expand() const { return EPtr(construct(a->expand())); }
 };
 
+// helper macro for toString; wraps S in parentheses if P>Q
 #define wrap(P, Q, S) (((P) > (Q)) ? std::string("(") : std::string()) + (S) + (((P) > (Q)) ? std::string(")") : std::string())
 
+// helper macro to declare simple unary operations, which are mostly the same
 #define UNARY_FUNCTION(Name, func, sfunc, extra) \
 struct Name : public UnaryOp { \
     Name(EPtr _a) : UnaryOp(std::move(_a)) { } \
@@ -196,11 +236,13 @@ struct Name : public UnaryOp { \
 #define FUNCTION_PRINTER(func) std::string toString(int prec = -1) const { return wrap(prec, Precedence::Func, std::string(#func "(") + a->toString(-1) + ")"); }
 #define SIMPLE_UNARY_FUNCTION(Name, func) UNARY_FUNCTION(Name, func, std::func, FUNCTION_PRINTER(func) )
 
+// need some extra stuff for Neg, as it's not a transcendental function
 UNARY_FUNCTION(Neg, -, -, bool polynomial(const Variable& var) const { return a->polynomial(var); } \
 EPtr simplify() const; \
 EPtr expand() const; \
 std::unique_ptr<Polynomial> facsum(const Variable&) const; \
 std::string toString(int prec = -1) const { return std::string("-") + a->toString(Precedence::Neg); })
+
 SIMPLE_UNARY_FUNCTION(Exp, exp)
 SIMPLE_UNARY_FUNCTION(Ln, log)
 SIMPLE_UNARY_FUNCTION(Sqrt, sqrt)
@@ -214,6 +256,7 @@ UNARY_FUNCTION(Gamma, gamma, gsl_sf_gamma, FUNCTION_PRINTER(gamma) )
 
 #undef UNARY_FUNCTION
 
+/// Abstract base class for binary operations.
 struct BinaryOp : public Expression {
     EPtr a, b;
     BinaryOp(EPtr _a, EPtr _b) : a(std::move(_a)), b(std::move(_b)) { }
@@ -224,6 +267,7 @@ struct BinaryOp : public Expression {
     EPtr expand() const { return EPtr(construct(a->expand(), b->expand())); }
 };
 
+// helper macro to declare mostly-identical binary operation classes
 #define BINARY_OP(Name, op, ladj, radj, expr, extra) \
 struct Name : public BinaryOp { \
     Name(EPtr _a, EPtr _b) : BinaryOp(std::move(_a), std::move(_b)) { } \
@@ -255,8 +299,9 @@ BINARY_OP(Div, /, 0, 1, _1 / _2, \
         std::unique_ptr<Polynomial> facsum(const Variable&) const;)
 BINARY_OP(Pow, ^, 1, 0, std::pow(_1, _2), bool polynomial(const Variable& var) const;)
 
+/// Exponentiation to a constant, integer power.
 struct PowInt : public UnaryOp {
-    int b;
+    int b; /// The power to raise to.
     PowInt(EPtr _a, int _b) : UnaryOp(std::move(_a)), b(_b) { }
     static std::unique_ptr<PowInt> create(EPtr a, int b) { return std::unique_ptr<PowInt>(new PowInt(std::move(a), b)); }
     Number evaluate() const;
@@ -272,9 +317,10 @@ struct PowInt : public UnaryOp {
     std::string toString(int prec = -1) const;
 };
 
-// left * var + right
+/// A recursively-defined polynomial in one variable.
+/// Equivalent to left * var + right.
 struct Polynomial : public Expression {
-    std::unique_ptr<Polynomial> left;
+    std::unique_ptr<Polynomial> left; // can be NULL; otherwise, must have left->var==var
     Variable var;
     EPtr right;
     Polynomial(std::unique_ptr<Polynomial> l, const Variable& v, EPtr r) : left(std::move(l)), var(v), right(std::move(r)) {
@@ -298,6 +344,7 @@ struct Polynomial : public Expression {
     std::string toString(int prec = -1) const;
 };
 
+/// Polygamma function; the b+1'th derivative of log(gamma(x)).
 struct PolyGamma : public UnaryOp {
     int b;
     PolyGamma(EPtr _a, int _b) : UnaryOp(std::move(_a)), b(_b) { }
@@ -310,12 +357,16 @@ struct PolyGamma : public UnaryOp {
     PolyGamma* construct(EPtr _a) const { return new PolyGamma(std::move(_a), b); }
     std::string toString(int prec = -1) const;
 };
+
+
+/// An equation.
 struct Equation : public Thing {
     EPtr a, b;
     Equation(EPtr _a, EPtr _b) : a(std::move(_a)), b(std::move(_b)) { }
     static std::unique_ptr<Equation> create(EPtr _a, EPtr _b) { return std::unique_ptr<Equation>(new Equation(std::move(_a), std::move(_b))); }
     std::string toString(int prec = -1) const { return wrap(prec, Precedence::Eq, a->toString(Precedence::Eq) + " = " + b->toString(Precedence::Eq)); }
 };
+/// An inequality, i.e. "a<b", "a<=b", "a>b", or "a>=b".
 struct Inequality : public Thing {
     EPtr a, b;
     enum Type {
@@ -377,6 +428,7 @@ struct Function : public Thing {
 };
 */
 
+// Operator overloads to make creating simple arithmetic expressions easier.
 inline EPtr operator-(EPtr a) {
     return Neg::create(std::move(a));
 }
